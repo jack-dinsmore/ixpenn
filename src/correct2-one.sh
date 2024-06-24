f5fa9e753d08d48c47fcb1bcb6580a6ee4c6ce4d
Jack Dinsmore <jtd@stanford.edu>
2024-06-24 11:15:32 -0700

    Updated attitude file:

--- src/correct2-one.sh
+++ src/correct2-one.sh
@@ -1,12 +1,12 @@
 ## Coordinate transformations.
-ixpedet2j2000 infile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w.fits outfile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000.fits attfile_in="$DATA_FOLDER"hk/ixpe"$SETNUM"_det"$DET"_att_v"$ATTNUM".fits clobber=True
+ixpedet2j2000 infile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w.fits outfile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000.fits attfile_in="$DATA_FOLDER"hk/ixpe"$OBS"_det"$DET"_att_v"$ATTNUM".fits clobber=True
 
 #######
 
 cp $DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000.fits $DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000_int.fits
 
 echo "About to start ixpeaspcorr"
-ixpeaspcorr infile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000.fits outfile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000_aspcorr.fits clobber=True n=300 attfile_in="$DATA_FOLDER"hk/ixpe"$SETNUM"_det"$DET"_att_v"$ATTNUM".fits attfile_out=$DATA_FOLDER"$FILENAME"_recon_att_corr.fits
+ixpeaspcorr infile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000.fits outfile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000_aspcorr.fits clobber=True n=300 attfile_in="$DATA_FOLDER"hk/ixpe"$OBS"_det"$DET"_att_v"$ATTNUM".fits attfile_out=$DATA_FOLDER"$FILENAME"_recon_att_corr.fits
 
 
 #######
@@ -14,4 +14,3 @@ ixpeaspcorr infile=$DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000.fits outf
 ftcopy $DATA_FOLDER"$FILENAME"_recon_nn_stokes_adj_w_j2000_aspcorr.fits'[EVENTS][col TRG_ID; TIME; STATUS; STATUS2; PI; W_MOM; W_NN; X; Y; Q; U]' $FINAL_FOLDER/"$FINAL_FILENAME".fits clobber=True
 
 #######
-
